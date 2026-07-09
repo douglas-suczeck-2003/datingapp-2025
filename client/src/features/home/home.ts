1edb68e6bc3a8a7c46e5c32086c3a1124fc45aaa
douglas-suczeck-2003 <douglassuczeck@outlook.com>
2026-07-08 22:12:28 -0300

    Updated home component

--- client/src/features/home/home.ts
+++ client/src/features/home/home.ts
@@ -2,6 +2,7 @@ import { Component, inject, Input, input, signal } from '@angular/core';
 import { Register } from "../account/register/register";
 import { User } from '../../types/user';
 import { Router, RouterLink } from "@angular/router";
+import { AccountService } from '../../core/services/account-service';
 
 @Component({
   selector: 'app-home',
@@ -11,6 +12,7 @@ import { Router, RouterLink } from "@angular/router";
 })
 export class Home {
   protected registerMode = signal(false);
+  protected accountService = inject(AccountService);
   router = inject(Router)
 
   showRegister(value: boolean) {
